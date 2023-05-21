8f9b9ad2378e43a48fb44442edad7498f94ff2a2
Íris <iris.balk04@gmail.com>
2023-05-21 20:41:27 -0300

    main
    Criando model cliente

--- gerenciamento_assistencia/app/models.py
+++ gerenciamento_assistencia/app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django_localflavor_br.br_states import STATE_CHOICES
 
 # Create your models here.
+
+class EnderecoTecnico(models.Model):
+    rua = models.CharField(max_length=50, null=False, blank=False)
+    cidade = models.CharField(max_length=30, null=False, blank=False)
+    estado = models.CharField(max_length=2, choices=STATE_CHOICES, null= False, blank=False)
+
+
+class Tecnico(models.Model):
+  nome = models.CharField(max_length=100, null=False, blank=False)
+  email = models.EmailField(null=False, blank=False)
+  endereco = models.ForeignKey(EnderecoTecnico, on_delete=models.CASCADE)
+  cpf = models.CharField(max_length=14, null=False, blank=False)
+  data_nascimento = models.DateField(null=False, blank=False)
+  profissao = models.CharField(max_length=25, null=False, blank=False)
